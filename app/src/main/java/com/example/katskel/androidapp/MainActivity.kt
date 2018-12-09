f21bfa4872c551dfc8464ce1bab744a183ce84d8
Katskel <katskel1999@tut.by>
2018-12-10 00:18:58 +0300

    show app version

--- app/src/main/java/com/example/katskel/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/katskel/androidapp/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.katskel.androidapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createLayout()
+    }
+
+    private fun createLayout() {
+        val versionName = BuildConfig.VERSION_NAME
+        var view = findViewById<TextView>(R.id.version_name)
+        view.text = versionName
     }
 }
