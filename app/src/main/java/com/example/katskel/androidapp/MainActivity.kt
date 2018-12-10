7c859340d3d24bccc2ec67406b9ff704f476b9f6
Katskel <katskel1999@tut.by>
2018-12-10 08:55:15 +0300

    add permission explanation

--- app/src/main/java/com/example/katskel/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/katskel/androidapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AlertDialog
 import android.telephony.TelephonyManager
 import android.widget.TextView
 
@@ -26,24 +27,24 @@ class MainActivity : AppCompatActivity() {
         var viewVersion = findViewById<TextView>(R.id.version_name)
         viewVersion.text = versionName
 
-        var viewImei = findViewById<TextView>(R.id.phone_imei)
-        viewImei.text = getImei().toString()
-
+        setViewImei()
     }
 
-    @SuppressLint("MissingPermission", "HardwareIds")
+    private fun setViewImei() {
+        var viewImei = findViewById<TextView>(R.id.phone_imei)
+        viewImei.text = getImei()
+    }
+
     fun getImei() : String {
-        var imei = "no info"
+        var imei = getString(R.string.no_info)
         val permission = ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.READ_PHONE_STATE)
 
-        //If the permission was denied show the dialog window to ask the permission
         if (permission != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     arrayOf(Manifest.permission.READ_PHONE_STATE),
                     MainActivity.REQUEST_PERMISSION_PHONE_STATE)
         }
-
         else {
             val telephonyManager = this.getSystemService(Context.TELEPHONY_SERVICE)
                     as TelephonyManager
@@ -53,4 +54,44 @@ class MainActivity : AppCompatActivity() {
         return imei
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MainActivity.REQUEST_PERMISSION_PHONE_STATE -> {
+                if ((grantResults.isNotEmpty() &&
+                                grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    setViewImei()
+                }
+
+                else if ((grantResults.isNotEmpty() &&
+                                grantResults[0] == PackageManager.PERMISSION_DENIED)) {
+                    showPermissionExplanation(Manifest.permission.READ_PHONE_STATE,
+                            getString(R.string.read_phone_state_explanation),
+                            MainActivity.REQUEST_PERMISSION_PHONE_STATE)
+                }
+                return
+            }
+        }
+    }
+
+    private fun showPermissionExplanation (permission : String, explanation : String,
+                                            permissionRequestCode: Int) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        permission)) {
+            val builder = AlertDialog.Builder(this)
+            val dialogQuestion = getString(R.string.permission_explanation_dialog_question)
+            builder.setMessage("$explanation $dialogQuestion")
+                    .setTitle(R.string.permission_explanation_dialog_title)
+
+            builder.setPositiveButton("Yes"){ _, _ ->
+            }
+                    .setNegativeButton("No") { _, _ ->
+                        ActivityCompat.requestPermissions(this,
+                                arrayOf(permission), permissionRequestCode)
+
+                    }
+
+            builder.show()
+        }
+    }
 }
